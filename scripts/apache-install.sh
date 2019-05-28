76a2ba93ca78a55fa68d948fd5b2a15fc06317c1
Laura Nicolas <lanicola@microsoft.com>
2019-05-28 17:36:51 +0200

    LinuxVM

--- scripts/apache-install.sh
+++ scripts/apache-install.sh
@@ -3,6 +3,18 @@
 yum -y update 
 # Then install apache server
 yum -y install httpd
+# Configure firewall and SELinux
+setenforce 0
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+# Add Hello World Message
+cat <<EOF > /var/www/html/index.html
+<html>
+<body>
+    <span wicket:id="message">Azure loves opensource</span>
+</body>
+</html>
+EOF
 # Start and configure automatic start for the service 
 systemctl enable httpd
 systemctl start httpd.service
